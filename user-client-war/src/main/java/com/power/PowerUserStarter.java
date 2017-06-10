ccaf2d75a3e0ae6def58b54d543d2d403928e3be
wuhun0301 <wuhun0301@qq.com>
2017-06-10 11:30:13 +0800

    init

--- user-client-war/src/main/java/com/power/PowerUserStarter.java
+++ user-client-war/src/main/java/com/power/PowerUserStarter.java
@@ -12,10 +12,10 @@ import org.springframework.boot.web.support.SpringBootServletInitializer;
 @SpringBootApplication
 @MapperScan("com.power.dao")
 public class PowerUserStarter extends SpringBootServletInitializer {
-    @Override
-    protected SpringApplicationBuilder configure(SpringApplicationBuilder application) {
-        return application.sources(PowerUserStarter.class);
-    }
+//    @Override
+//    protected SpringApplicationBuilder configure(SpringApplicationBuilder application) {
+//        return application.sources(PowerUserStarter.class);
+//    }
 
     public static void main(String[] args) throws Exception {
         SpringApplication.run(PowerUserStarter.class, args);
