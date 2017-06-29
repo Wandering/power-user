ff1d33877758717ee4192abc6410004789a8475b
wuhun0301 <wuhun0301@qq.com>
2017-06-29 22:42:52 +0800

    新用户不能注册的问题

--- user-client-war/src/main/java/com/power/config/InterceptorContextConfig.java
+++ user-client-war/src/main/java/com/power/config/InterceptorContextConfig.java
@@ -25,8 +25,6 @@ public class InterceptorContextConfig extends WebMvcConfigurerAdapter {
 
     @Override
     public void addInterceptors(InterceptorRegistry registry) {
-        registry.addInterceptor(crossInterceptor)
-                .addPathPatterns("/**");
         //登录处理
         registry.addInterceptor(loginInterceptor)
                 .addPathPatterns("/**")
