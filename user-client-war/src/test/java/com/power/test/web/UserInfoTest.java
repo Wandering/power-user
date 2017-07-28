1ee6cb82e305dd7f82f32b65c4ac4519677a8ea8
wuhun0301 <wuhun0301@qq.com>
2017-07-29 01:08:36 +0800

    服务性能优化

--- user-client-war/src/test/java/com/power/test/web/UserInfoTest.java
+++ user-client-war/src/test/java/com/power/test/web/UserInfoTest.java
@@ -112,12 +112,6 @@ public class UserInfoTest extends BaseTest{
         Assert.isTrue(userInfoDTO.getOpenId().equals(openId),"openId错误");
 
     }
-
-    @Test
-    public void getSmsCheckCode(){
-        String checkCode = redis.get(checkKey);
-        logger.info("checkCode===================={}",checkCode);
-    }
     @Test
     public void testQueryWxPlatform() throws Exception {
 
